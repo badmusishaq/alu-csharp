a51c381801af8ce82c31b231cf07bb657f6a9211
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-01-24 13:10:25 +0100

    Updating with interfaces

---
+++ csharp-arrays_lists_dictionaries/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//List<int> newList;
+
+//newList = new List<int>();
+
+//int addedNumber = 0;
+
+//addedNumber++;
+CreatePrint(10);
+
+void CreatePrint(int size)
+{
+    int[] numbersToPrint;
+
+    numbersToPrint.Length = size;
+
+    if(size < 0)
+    {
+        Console.WriteLine("Size cannot be negative");
+        return;
+    }
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine(numbersToPrint[i]);
+    }    
+}
+
+Console.WriteLine("List Length: " + CreatePrint(10));
---
+++ csharp-interfaces/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine();
+
+public abstract class Base
+{
+    Fish whaleFish = new Fish();
+
+    public string name = "Whale";
+}
+
+class Fish : Base
+{
+    public string newName = "New Whale";
+}
