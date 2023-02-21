b05e055d6dd19eb2c0ff9177d520ce9df57bd288
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-21 15:21:02 +0100

    Editing strings

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -4,8 +4,8 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		string str1 = "Holberton";
-		string str2 = " School";
+		string str1 = "Holberton ";
+		string str2 = "School";
 		str1 += str2;
 		Console.WriteLine("Welcome to {0}!", str1);
 	}
