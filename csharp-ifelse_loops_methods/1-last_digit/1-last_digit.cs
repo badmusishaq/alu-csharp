96fa24ce655763aa1f80b7bcd0e209e4dc28a3d3
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-21 20:11:41 +0100

    Edited

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -6,6 +6,18 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-	// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+        int lastNumber = Math.Abs(number) % 10;
+        if(lastNumber > 5)
+        {
+            Console.WriteLine("The last digit of " + number + " is " + lastNumber + " and is greater than 5");
+        }
+        else if(lastNumber == 0)
+        {
+            Console.WriteLine("The last digit of " + number + " is " + lastNumber + " and is 0");
+        }
+        else if(lastNumber < 6 && lastNumber != 0)
+        {
+            Console.WriteLine("The last digit of " + number + " is " + lastNumber + " and is less than 6 and not 0");
+        }
     }
 }
---
+++ csharp-ifelse_loops_methods/README.md
@@ -0,0 +1,5 @@
+# If/Else Methods
+Testing if else projects.
+
+## Other Topics
+This include Methods and and loops.
