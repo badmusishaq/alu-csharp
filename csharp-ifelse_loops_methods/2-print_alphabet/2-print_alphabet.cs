a98d467dbaf33764b8ac33c36b3efc93a0bf503b
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-21 22:04:01 +0100

    Char updated

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (char ch = 'a'; ch <= 'z'; ch++)
+    {
+        //Printing all alphabets with tab
+        if(ch =='e' || ch == 'q')
+        {
+            continue;
+        }
+        Console.Write(ch+"\t");
+        
+    }
+//Console.WriteLine("Hello, World!");
