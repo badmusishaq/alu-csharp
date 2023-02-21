a3e3406cbaeadf22cb8f169e6e211c6e611b5746
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-21 15:34:26 +0100

    Editing strings

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-int number = 972;
-Console.WriteLine(number + " Mission Street");
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine(number + " Mission Street");
+	}
+}
