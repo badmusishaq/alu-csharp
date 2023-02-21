9ddf1fafe5c68300557c5bae57aeebab67a31f4f
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-21 19:53:08 +0100

    Editing String

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -1,15 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Random rndm = new Random();
-int number = rndm.Next(-10, 10);
-if(number < 0)
+﻿using System;
+
+class Program
 {
-    Console.WriteLine(number + " is negative");
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+	if(number > 0)
+    {
+        Console.WriteLine(number + " is positive");
+    }
+    else if(number < 0)
+    {
+        Console.WriteLine(number + " is negative");
+    }
+    else{
+        Console.WriteLine(number + " is zero");
+    }
+    }
 }
-else if(number == 0)
-{
-    Console.WriteLine(number + " is zero");
-}
-else if(number > 0)
-{
-    Console.WriteLine(number + " is positive");
-}
+
