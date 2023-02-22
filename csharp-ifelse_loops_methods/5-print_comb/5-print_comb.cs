27fcd680c9989d3f38e874a285534c5dc349921d
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-22 12:52:45 +0100

    Added

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("00");
+for(int i = 1; i <= 99; i++)
+{
+    Console.Write(", {0}", i.ToString("00"));
+}
+Console.WriteLine();
