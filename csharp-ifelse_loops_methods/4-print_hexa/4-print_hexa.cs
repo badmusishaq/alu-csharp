83bb247ecca5ca79bd259d8d3fbdbb45c7d15f5d
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-22 03:33:56 +0100

    Update 4-print_hexa.cs

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -2,9 +2,5 @@
 
 for(int n = 0; n <= 98; n++)
 {
-    //int decValue = int.Parse(n.ToString("X"), System.Globalization.NumberStyles.HexNumber);
-    //Console.Write(n + " = " + Convert.ToInt32(n.ToString(), 16)+"\t");
-    //Console.Write(n + " = " + decValue + "\t");
     Console.Write(n + " = " + string.Format("{0:X}", n) + "\t");
 }
-//Console.WriteLine("Hello, World!");
