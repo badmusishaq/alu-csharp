dd1598dbb862991a699f41eb1b9a99dcd1f78456
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-24 04:08:23 +0100

    Update 6-print_comb2.cs

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -6,22 +6,15 @@ namespace _6_print_comb2
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 9; i ++)
-                for (int j = 0; j < 10; j++)
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
                 {
-                    if (j > i)
-                    {
-                        if (i < 8 || j < 9)
-                        {
-                            Console.Write(i+""+j + ", ");
-                        }
-                        else
-                        {
-                            Console.Write(i+""+j);
-                        }
-                    }                      
+                    Console.Write(i + "" + j + ", ");
                 }
-            Console.WriteLine();
+            }
+
+            Console.WriteLine("89");
         }
     
     }
