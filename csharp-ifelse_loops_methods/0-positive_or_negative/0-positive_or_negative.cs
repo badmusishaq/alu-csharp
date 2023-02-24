41b0b3cdc4eb6492515ef9abc0c455800db5c191
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-24 01:57:55 +0100

    Edited

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -6,17 +6,18 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10, 10);
-	if(number > 0)
-    {
-        Console.WriteLine(number + " is positive");
-    }
-    else if(number < 0)
-    {
-        Console.WriteLine(number + " is negative");
-    }
-    else{
-        Console.WriteLine(number + " is zero");
-    }
+        if(number > 0)
+        {
+            Console.WriteLine(number + " is positive");
+        }
+        else if(number == 0)
+        {
+            Console.WriteLine(number + " is zero");
+        }
+        else
+        {
+            Console.WriteLine(number + " is negative");
+        }
     }
 }
 
