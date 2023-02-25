15d1b9cc38de8de047086aee09abfa45ed60679c
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-02-25 04:52:27 +0100

    setting up tdd

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,23 @@
+using MyMath;
+
+namespace MyMath.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    [TestCase(40, 10)]
+    [TestCase(37, 13)]
+    [TestCase(25, 25)]
+    [TestCase(50, 0)]
+    [TestCase(100, -50)]
+    public void SimpleAdditionOperations(int value, int valueb)
+    {
+        var result = MyMath.Operations.Add(value, valueb);
+        Assert.AreEqual(50, result);
+    }
+}
---
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>Represents Mathemathics operations class.</summary>
+    public class Operations
+    {
+        public static int Add(int a, int b){
+            return a + b;
+        }
+    }
+}
