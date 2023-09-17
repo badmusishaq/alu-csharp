e73a5b28df695a688b2e8a03f2e01a11977a3809
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-09-17 16:52:01 +0100

    Update 0-print_array.cs

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,22 +9,26 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        if(size == 0)
-        {
-            Console.WriteLine("\t");
-        }
-        else if(size < 0)
+        if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
-            return new int[0];
+            return null;
         }
 
-        int[] newSize = new int[size];
-
-        for(int i = 0; i < size; i++)
+        int[] tab = new int[size];
+        var output = new StringBuilder();
+        for (int i = 0; i < size; i++)
         {
-            Console.WriteLine("{0} ", i);
+            output.Append(i);
+            if (i != size - 1)
+            {
+                output.Append(" ");
+            }
+            tab[i] = i;
         }
-        return newSize;
+
+        Console.WriteLine(output);
+
+        return tab;
     }
 }
