69a4968537e4b985de50dc3a20feec2282c3dc32
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-09-23 18:07:53 +0100

    Update 7-concat.cs

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -4,7 +4,7 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		string str1 = "Mission ";
+		string str1 = "Holberton ";
 		string str2 = "School";
 		str1 += str2;
 		Console.WriteLine("Welcome to {0}!", str1);
