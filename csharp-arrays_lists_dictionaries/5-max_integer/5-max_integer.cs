520d756512facd0bd31e8efd7eaf1a89e493538f
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-09-27 00:49:25 +0100

    Update 5-max_integer.cs

--- csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 
-List<int> myList1 = new List<int>() {98, -10, 0, 32, 972, 12, 1024, -727, -98, 50, 8};
-        List<int> myList2 = new List<int>() {98, 972, 971, -32, 972, 972, -727};
-
-        Console.WriteLine("Max: " + List.MaxInteger(myList1));
-        Console.WriteLine("Max: " + List.MaxInteger(myList2));
-
 class List
 {
     public static int MaxInteger(List<int> myList)
