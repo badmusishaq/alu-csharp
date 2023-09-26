0805c3a6b1ac5e1a1aa7d6550cbb66fa9f588571
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-09-26 18:15:25 +0100

    Update 1-element_at.cs

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,9 +1,5 @@
-﻿int[] array = {10, 17, -8, 4, -12, 7, 0, 1, -1, -9};
+﻿using System;
 
-        Console.WriteLine("Element at index {0} is {1}", 4, Array.elementAt(array, 4));
-        Console.WriteLine("Element at index {0} is {1}", 9, Array.elementAt(array, 9));
-        Console.WriteLine("Element at index {0} is {1}", 0, Array.elementAt(array, 0));
-        Array.elementAt(array, -7);
 class Array
 {
     public static int elementAt(int[] array, int index)
