6cac0193314997c86cf9a95416f1e4685a7025fe
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-09-30 00:34:29 +0100

    Update 8-number_keys.cs

--- csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -1,13 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
-Dictionary<string, string> myDict = new Dictionary<string, string>();
-
-        myDict.Add("language", "C");
-        myDict.Add("track", "low level");
-        myDict.Add("school", "Holberton");
-
-        Console.WriteLine("Number of keys: {0}", Dictionary.NumberOfKeys(myDict));
         
 class Dictionary
 {
