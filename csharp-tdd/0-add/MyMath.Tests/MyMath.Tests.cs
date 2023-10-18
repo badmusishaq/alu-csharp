d9be19fa04dfe669a888c5f1a30b51ffb89c649a
Isiaq Gbadamosi <badmusishaq@gmail.com>
2023-10-18 15:48:57 +0100

    Updated

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,23 +1,25 @@
+using NUnit.Framework;
 using MyMath;
 
-namespace MyMath.Tests;
-
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        [SetUp]
+        public void Setup()
+        {
+        }
 
-    [Test]
-    [TestCase(40, 10)]
-    [TestCase(37, 13)]
-    [TestCase(25, 25)]
-    [TestCase(50, 0)]
-    [TestCase(100, -50)]
-    public void SimpleAdditionOperations(int value, int valueb)
-    {
-        var result = MyMath.Operations.Add(value, valueb);
-        Assert.AreEqual(50, result);
+        [Test]
+        [TestCase(40, 10)]
+        [TestCase(37, 13)]
+        [TestCase(25, 25)]
+        [TestCase(50, 0)]
+        [TestCase(100, -50)]
+        public void SimpleAdditionOperations(int value, int valueb)
+        {
+            var result = MyMath.Operations.Add(value, valueb);
+            Assert.AreEqual(50, result);
+        }
     }
 }
