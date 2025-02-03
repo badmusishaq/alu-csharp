f257ed188a0142a408bc15ebeacecb6b132341e1
Isiaq Gbadamosi <badmusishaq@gmail.com>
2025-02-03 14:53:45 +0200

    Updated

--- csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -5,20 +5,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[][] jaggedArray = new int[3][];
-
-        jaggedArray[0] = new int[] { 0, 1, 2, 3 };
-        jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
-        jaggedArray[2] = new int[] { 0, 1 };
-
-        for (int i = 0; i < jaggedArray.Length; i++)
+        int[][] jaggerArr =
         {
-            Console.Write("Array {i} : ");
-            for (int j = 0; j < jaggedArray[i].Length; j++)
-            {
-                Console.Write(jaggedArray[i][j] + " ");
+            new int[] { 0, 1, 2, 3 },
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+        };
+
+        var output = new StringBuilder();
+        for (int i = 0; i < jaggerArr.Length; i++){
+            output.Clear();
+            for (int b = 0; b < jaggerArr[i].Length; b++){
+                output.Append(jaggerArr[i][b]);
+                if (jaggerArr[i].Length - 1 != b){
+                    output.Append(" ");
+                }
             }
-            Console.WriteLine();
+            Console.WriteLine(output);
         }
+
     }
 }
